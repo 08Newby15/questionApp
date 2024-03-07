c8d8c47d467b1719a255d543338ff87d6b804d1f
08Newby15 <130486834+08Newby15@users.noreply.github.com>
2024-03-07 12:30:16 +0100

    Command folter with input

---
+++ src/QuestionsApp.Web/Handlers/Commands/VoteForQuestionCommand.cs
@@ -0,0 +1,17 @@
+namespace QuestionsApp.Web.Handlers.Commands;
+using MediatR;
+
+public class VoteForQuestionRequest : IRequest<IResult>
+{
+    public int QuestionId { get; set; }
+}
+
+public class VoteForQuestionCommand : IRequestHandler<VoteForQuestionRequest, IResult>
+{
+    public Task<IResult> Handle(VoteForQuestionRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
